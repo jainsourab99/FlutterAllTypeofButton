0f351eb465add5dbc27c3a1ab8e1932d4e97bf31
Sourabh Jain <42956699+jainsourab99@users.noreply.github.com>
2024-03-22 10:22:10 +0530

    Text Field Input Widget

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
         textTheme: const TextTheme(
             displayLarge: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
             titleMedium: TextStyle(fontSize: 14, fontWeight: FontWeight.w500)),
@@ -46,6 +46,8 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  var emailText = TextEditingController();
+  var passwordText = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -57,21 +59,66 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        backgroundColor: Theme.of(context).colorScheme.onSecondary,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: const Text("Flutter Widgets"),
       ),
       body: Center(
-        child: Card(
-          shadowColor: Colors.blue,
-          elevation: 12,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              "Hello World",
-              style: mTextStyle18(),
-            ),
+        child: Container(
+          width: 300,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                controller: emailText,
+                enabled: true,
+                decoration: InputDecoration(
+                    hintText: "Enter Your email",
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide: BorderSide(color: Colors.purple, width: 2)),
+                    enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide: BorderSide(color: Colors.blue, width: 2)),
+                    disabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide: BorderSide(color: Colors.grey, width: 5)),
+                    // suffixText: "UserName Exist",
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        Icons.remove_red_eye,
+                        color: Colors.orange,
+                      ),
+                      onPressed: () {},
+                    ),
+                    prefixIcon: Icon(Icons.email)),
+              ),
+              Container(
+                height: 16,
+              ),
+              TextField(
+                keyboardType: TextInputType.multiline,
+                controller: passwordText,
+                obscureText: true,
+                obscuringCharacter: "*",
+                decoration: InputDecoration(
+                  hintText: "Enter Your password",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                    borderSide: BorderSide(color: Colors.deepOrange),
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    String uname = emailText.text.toString();
+                    String pass = passwordText.text;
+                    print("Email: $uname, pass: $pass");
+                  },
+                  child: Text("Login"))
+            ],
           ),
         ),
       ),
